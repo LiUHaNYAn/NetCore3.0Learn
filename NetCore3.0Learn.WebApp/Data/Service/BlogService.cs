0d81d5ba58ffca9db5034a1b078094d4dc005a46
admin <why3046380@163.com>
2019-10-16 17:04:22 +0800

    抽象service接口

--- NetCore3.0Learn.WebApp/Data/Service/BlogService.cs
+++ NetCore3.0Learn.WebApp/Data/Service/BlogService.cs
@@ -1,10 +1,31 @@
-﻿namespace NetCore3._0Learn.WebApp.Data.Service
+﻿using System;
+using NetCore3._0Learn.WebApp.Data.Model;
+using NetCore3._0Learn.WebApp.Data.Repository;
+
+namespace NetCore3._0Learn.WebApp.Data.Service
 {
-    public class BlogService:IBlogService
+    public class BlogService : ServiceBase, IBlogService
     {
+        private readonly IRepository<Blog, Guid> _blogRepository;
+        private IRepository<Post, Guid> _postRepository;
+
+        public BlogService(IDataBase dataBase, IRepository<Blog, Guid> blogRepository,
+            IRepository<Post, Guid> postRepository) : base(dataBase)
+        {
+            _blogRepository = blogRepository;
+            _postRepository = postRepository;
+        }
+
         public void Add()
         {
-            
+            var data=_blogRepository.GetModel(Guid.Parse("CABC53ED-91E5-4CD3-9BDE-038EDE9209B6"));
+            DataBase.BeginTran();
+            var blogid = Guid.NewGuid();
+//            postRepository.AddEntity(new Post {Id = Guid.NewGuid(), Title = "demo176FF941-7 ",BlogId = blogid});
+            _blogRepository.AddEntity(new Blog {Id = Guid.NewGuid(), CreateTime = DateTime.Now, Title = "2fsfsd"});
+            _blogRepository.AddEntity(new Blog
+                {Id = blogid, CreateTime = DateTime.Now, Title = "helTransactionId:Transa"});
+            DataBase.Commit();
         }
     }
 }
